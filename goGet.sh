4d80d70ff950cfbc45d2991ee433bf8fcd3d7a37
Evandro Pezzi <evandropp10@gmail.com>
2019-01-14 18:51:48 -0200

    Add files via upload

---
+++ goGet.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+go get github.com/dgrijalva/jwt-go
+go get github.com/gorilla/context
+go get github.com/gorilla/handlers
+go get github.com/gorilla/mux
+go get github.com/mitchellh/mapstructure
+go get google.golang.org/appengine
+go get cloud.google.com/go/datastore
+go get golang.org/x/sys/unix
